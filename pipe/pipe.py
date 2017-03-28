c8bba018a5e01f4a73d02f00f0ebe999fa598301
tamfr <ejm29@case.edu>
2017-03-28 17:40:30 -0500

    Enhanced readability where there was naught

--- pipe/pipe.py
+++ pipe/pipe.py
@@ -4,42 +4,104 @@ Created on Wed Feb  5 14:19:08 2014
 
 @author: Mott
 
-Numerical root finder using Newton's method for systems of equations with form Ax+Bx^2+C.
+Numerical root finder using Newton's method for systems of equations with form 
+Ax+Bx^2+C.
 """
-# Imports necessary math modules for linear algebra, sig figs, division, and importing from Excel.
+# Imports necessary math modules for linear algebra, sig figs, division, and 
+# importing from Excel.
 
 from __future__ import division
 from math import floor, log10, pi
 from numpy import matrix, zeros, ones, multiply
 from pysnatch import xlsnatch
 import os
-mypath = os.path.dirname(__file__) # Makes available the absolute path to the directory of this file such that if it is imported and gets executed outside its original directory, any subsequently imported files can be found.
+
+# Makes available the absolute path to the directory of this file such that if 
+# it is imported and gets executed outside its original directory, any 
+# subsequently imported files can be found.
+
+mypath = os.path.dirname(__file__) 
 
 def pipe():
     """
-    Solves systems of equations to determine mass flow rates and pressures in a fluidics network.
+    Solves systems of equations to determine mass flow rates and pressures in a 
+    fluidics network.
     """ 
     total_unknowns = 148 # Total number of unkown variables.  
     file_name = 'coefficients2.xlsx'
-    variable_names = xlsnatch(os.path.join(mypath,file_name),1,2,2,1,total_unknowns+1,"list") # Import variable names from Excel as list. 
+    variable_names = xlsnatch(
+        os.path.join( mypath, file_name ),
+        1,
+        2,
+        2,
+        1,
+        total_unknowns+1,
+        "list",
+        ) # Import variable names from Excel as list. 
+    
+    # Determine how many pressure unknowns there are.  
     num_pressure_unknowns = 0 # Initiate variable.
+    
     for name in variable_names[0][:]:
-        num_pressure_unknowns += (name[:1].encode('ascii','ignore')=="P") # Determine how many pressure unknowns there are.
+              
+        num_pressure_unknowns += ( name[:1].encode('ascii', 'ignore') == "P" ) 
            
-    Diameters = xlsnatch(os.path.join(mypath,file_name),0,12,2,1,total_unknowns-num_pressure_unknowns+1,"matrix") # Snatch diameters [m] for later calculations.
+    Diameters = xlsnatch(
+        os.path.join(mypath, file_name),
+        0,
+        12,
+        2,
+        1,
+        total_unknowns-num_pressure_unknowns+1,
+        "matrix",
+        ) # Snatch diameters [m] for later calculations.
+    
     rho = 1000 # Density of water [kg/m^3].
     
     # Set coefficients for equation Ax+Bx^2+C.
-    A = matrix(zeros((total_unknowns,total_unknowns))) # Initiate matrix.
-    B = matrix(zeros((total_unknowns,total_unknowns))) # Initiate matrix.
-    C = matrix(zeros((len(A),1))) # Initiate matrix.
-    A = xlsnatch(os.path.join(mypath,file_name),1,3,2,total_unknowns,total_unknowns,"matrix") # Imports data from Excel file by using absolute path to file to propagate matrix of coefficients.
-    B = xlsnatch(os.path.join(mypath,file_name),2,3,2,total_unknowns,total_unknowns,"matrix") # Imports data from Excel file by using absolute path to file to propagate matrix of coefficients.
-    C = xlsnatch(os.path.join(mypath,file_name),3,3,2,total_unknowns,1,"matrix") # Imports data from Excel file by using absolute path to file to propagate matrix of coefficients.
+    A = matrix(zeros(( total_unknowns, total_unknowns ))) # Initiate matrix.
+    B = matrix(zeros(( total_unknowns, total_unknowns ))) # Initiate matrix.
+    C = matrix(zeros(( len(A), 1 ))) # Initiate matrix.
     
-    f=lambda x: A*x+B*multiply(x,x)+C # Define function f to solve for roots.
+    # Imports data from Excel file by using absolute path to file to propagate 
+    # matrix of coefficients.
+    A = xlsnatch(
+        os.path.join( mypath, file_name ),
+        1,
+        3,
+        2,
+        total_unknowns,
+        total_unknowns,
+        "matrix",
+        ) 
+        
+    B = xlsnatch(
+        os.path.join( mypath, file_name ),
+        2,
+        3,
+        2,
+        total_unknowns,
+        total_unknowns,
+        "matrix",
+        ) 
+   
+    C = xlsnatch(
+        os.path.join( mypath, file_name ),
+        3,
+        3,
+        2,
+        total_unknowns,
+        1,
+        "matrix",
+        )
+        
+    # Define function f to solve for roots.
+    f = lambda x: A*x + B*multiply(x,x) + C 
     
-    J = matrix(zeros((A.shape[0],A.shape[1]))) # Initiate Jacobian based on number of equations as defined by matrix A.
+    # Initiate Jacobian based on number of equations as defined by matrix A.
+    J = matrix(
+        zeros(( A.shape[0], A.shape[1] ))
+        ) 
     
     # Define parameters to run loop
     n = 0 # Define iteration counter.
@@ -47,11 +109,11 @@ def pipe():
     tol = 2e-11 # Define tolerance upon which to interate.
     
     # Initial guesses
-    vel_guess = .0000003 # Good guest found to be .0000003 to yield all positive roots.
+    vel_guess = .0000003 # Good guess is .0000003 to yield all positive roots.
     pressure_guess = 103421.25
     
-    X = matrix(ones((A.shape[0],1))*pressure_guess)
-    X[num_pressure_unknowns:X.shape[0],0] = vel_guess
+    X = matrix(ones((A.shape[0], 1))*pressure_guess)
+    X[ num_pressure_unknowns:X.shape[0], 0 ] = vel_guess
     #X = matrix('[-5;7;-3]')
     
     # Newton's method for finding roots
@@ -64,7 +126,8 @@ def pipe():
             
         X = X - J.I*f(X)
         
-        #X = matrix([(abs(X[x,0]) if X[x,0] < 0 else X[x,0]) for x in range(0,len(X))]).T
+        #X = matrix([(abs(X[x,0]) if X[x,0] < 0 else X[x,0]) for x in 
+        #range(0,len(X))]).T
         
         if n < max_iterations:
             n = n + 1
@@ -74,15 +137,69 @@ def pipe():
     pressure_precision = 10 # Define number of sig figs.  
     velocity_precision = 5 # Define number of sig figs. 
     ans = []    
-    ans[0:num_pressure_unknowns] = [round(x,-int(floor(log10(abs(x)/10**pressure_precision)))) for x in X[0:num_pressure_unknowns,0]] # Round numbers to certain number of sig figs based on precision      
-    ans[num_pressure_unknowns:X.shape[0]] = [round(x,-int(floor(log10(abs(x)/10**velocity_precision)))) for x in X[num_pressure_unknowns:X.shape[0],0]] # Round numbers to certain number of sig figs based on precision
-    ans.append(round(C[len(C)-1,0]/Diameters[0,len(C)-num_pressure_unknowns]**2,-int(floor(log10(abs(C[len(C)-1,0]/Diameters[0,len(C)-num_pressure_unknowns]**2)/10**velocity_precision))))) # Append the given velocity
+    pressure_sig_figs = lambda x: round(
+        x,
+        -int(
+            floor(
+                log10( abs(x) / 10**pressure_precision )
+                )
+            )
+        ) 
+        
+    velocity_sig_figs = lambda x: round(
+        x,
+        -int(
+            floor(
+                log10( abs(x) / 10**velocity_precision )
+                )
+            )
+        ) 
     
-    m_dot = [round(x,-int(floor(log10(abs(x)/10**velocity_precision)))) for x in multiply(multiply(Diameters[0,:-1].T,Diameters[0,:-1].T),X[num_pressure_unknowns:,0])*pi*rho/4] # Calculate mass flow rates in each branch.
-    m_dot.append(round(ans[len(ans)-1]*Diameters[0,len(C)-num_pressure_unknowns]**2*pi*rho/4,-int(floor(log10(abs(ans[len(ans)-1]*Diameters[0,len(C)-num_pressure_unknowns]**2*pi*rho/4)/10**velocity_precision))))) # Append the calculated mass flow rate from given velocity
+    # Round numbers to certain number of sig figs based on precision 
+    ans[ 0:num_pressure_unknowns ] = [
+        pressure_sig_figs(x) for x in X[ 0:num_pressure_unknowns, 0 ]
+        ]      
+    
+    ans[ num_pressure_unknowns:X.shape[0] ] = [
+         velocity_sig_figs(x) for x in X[ num_pressure_unknowns:X.shape[0], 0 ]
+        ] 
+    
+    # Append the given velocity
+    ans.append(
+        round(
+            C[len(C)-1,0]/Diameters[0,len(C)-num_pressure_unknowns]**2,
+            -int(
+                floor(
+                    log10(
+                        abs(C[len(C)-1,0]/Diameters[0,len(C)-num_pressure_unknowns]**2)/10**velocity_precision
+                        )
+                    )
+                )
+            )
+        ) 
+        
+    # Calculate mass flow rates in each branch.
+    m_dot = [
+        velocity_sig_figs(x) for x in multiply(multiply(Diameters[0,:-1].T,Diameters[0,:-1].T),X[num_pressure_unknowns:,0])*pi*rho/4
+        ]
+    
+    # Append the calculated mass flow rate from given velocity
+    m_dot.append(
+        round(
+            ans[len(ans)-1]*Diameters[0,len(C)-num_pressure_unknowns]**2*pi*rho/4,
+            -int(
+                floor(
+                    log10(
+                        abs(ans[len(ans)-1]*Diameters[0,len(C)-num_pressure_unknowns]**2*pi*rho/4)/10**velocity_precision
+                        )
+                    )
+                )
+            )
+        ) 
     
     
     variables = {}
+    
     for i in range(0,len(ans)):
         variables[variable_names[0][i].encode('ascii','ignore')] = ans[i]
         if i >= num_pressure_unknowns:
